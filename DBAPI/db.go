6e3e25f88cff2d360137b318ebdffdcea91cfc59
Sayan Mallick <snyxmk@gmail.com>
2024-06-07 01:32:40 +0530

    JSONResult implementation

--- DBAPI/db.go
+++ DBAPI/db.go
@@ -14,8 +14,13 @@ type DBAPI struct {
 	schema string
 }
 
+type JSONResult struct {
+	O map[string]interface{}
+	A []map[string]interface{}
+}
+
 func NewDBAPI(schema string) *DBAPI {
-	conn, err := pgx.Connect(context.Background(), "dbname=s user=s")
+	conn, err := pgx.Connect(context.Background(), "host=127.0.0.1 dbname=s user=s")
 	if err != nil {
 		log.Fatalf("Unable to connect to database: %v", err)
 	}
@@ -26,7 +31,7 @@ func NewDBAPI(schema string) *DBAPI {
 	}
 }
 
-func (db *DBAPI) a(funcName string, params ...interface{}) (bool, []byte) {
+func (db *DBAPI) a(funcName string, params ...interface{}) (bool, JSONResult) {
 	qs := "("
 	for i := range params {
 		if i > 0 {
@@ -43,104 +48,53 @@ func (db *DBAPI) a(funcName string, params ...interface{}) (bool, []byte) {
 
 	err := db.conn.QueryRow(context.Background(), sql, params...).Scan(&ok, &js)
 	if err != nil {
-		return false, nil
+		return false, JSONResult{}
 	}
 
-	return ok, js
-}
+	var result interface{}
+	err = json.Unmarshal(js, &result)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-type Person struct {
-	ID        int     `json:"id"`
-	CreatedAt string  `json:"created_at"`
-	Admin     bool    `json:"admin"`
-	Name      string  `json:"name"`
-	Things    []Thing `json:"things"`
-}
+	switch result.(type) {
+	case []interface{}:
+		// convert []interface{} to []map[string]interface{}
+		var array []map[string]interface{}
+		for _, item := range result.([]interface{}) {
+			array = append(array, item.(map[string]interface{}))
+		}
+		return true, JSONResult{A: array}
+	case map[string]interface{}:
+		return true, JSONResult{O: result.(map[string]interface{})}
+	}
 
-type Thing struct {
-	ID        int     `json:"id"`
-	Person_IS int     `json:"person_id"`
-	Name      string  `json:"name"`
-	Price     float32 `json:"price"`
-}
-
-type Error struct {
-	Error string `json:"error"`
+	return false, JSONResult{}
 }
 
 func main() {
 	API := NewDBAPI("zz")
 
-	var person Person
-	var error Error
-	var people []Person
-	var thing Thing
-
 	fmt.Println("PERSON:")
-	ok, data := API.a("person", 1)
-	if !ok {
-		json.Unmarshal(data, &error)
-		fmt.Println(error)
-	} else {
-		json.Unmarshal(data, &person)
-		fmt.Println(person)
-	}
-
-	ok, data = API.a("person", 2)
-	if !ok {
-		json.Unmarshal(data, &error)
-		fmt.Println(error)
-	} else {
-		json.Unmarshal(data, &person)
-		fmt.Println(person)
-	}
-
-	ok, data = API.a("person", 999)
-	if !ok {
-		json.Unmarshal(data, &error)
-		fmt.Println(error)
-	} else {
-		json.Unmarshal(data, &person)
-		fmt.Println(person)
-	}
+	_, person := API.a("person", 1)
+	fmt.Println(person.O["name"])
+	_, person = API.a("person", 2)
+	fmt.Println(person.O["name"])
+	_, person = API.a("person", 999)
+	fmt.Println(person.O["error"])
 
 	fmt.Println("PEOPLE:")
-	ok, data = API.a("people")
-	if !ok {
-		json.Unmarshal(data, &error)
-		fmt.Println(error)
-	} else {
-		json.Unmarshal(data, &people)
-		fmt.Println(people)
-	}
+	_, people := API.a("people")
+	fmt.Println(people.A[0]["name"])
 
 	fmt.Println("GIVE CHARLIE CHOCOLATE: (only once)")
-	ok, data = API.a("thing_add", 2, "chocolate", 3.75)
-	if !ok {
-		json.Unmarshal(data, &error)
-		fmt.Println(error)
-	} else {
-		json.Unmarshal(data, &thing)
-		fmt.Println(thing.ID)
-	}
-	if ok {
-		ok, data = API.a("thing_add", 2, "chocolate", 3.75)
-		if !ok {
-			json.Unmarshal(data, &error)
-			fmt.Println(error)
-		} else {
-			json.Unmarshal(data, &thing)
-			fmt.Println(thing.ID)
-		}
-	}
+	_, thing := API.a("thing_add", 2, "chocolate", 3.75)
+	fmt.Println(thing)
+
+	_, thing = API.a("thing_add", 2, "chocolate", 3.75)
+	fmt.Println(thing)
 
 	fmt.Println("GIVE CHARLIE LOVE: (overloaded, priceless)")
-	ok, data = API.a("thing_add", 2, "love")
-	if !ok {
-		json.Unmarshal(data, &error)
-		fmt.Println(error)
-	} else {
-		json.Unmarshal(data, &thing)
-		fmt.Println(thing.ID)
-	}
+	_, thing = API.a("thing_add", 2, "love")
+	fmt.Println(thing)
 }
