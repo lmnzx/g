cbd0ce37264ca13b9403f095c9889016a0076c73
Sayan Mallick <snyxmk@gmail.com>
2024-06-07 09:42:45 +0530

    using generics

--- DBAPI/db.go
+++ DBAPI/db.go
@@ -31,7 +31,7 @@ func NewDBAPI(schema string) *DBAPI {
 	}
 }
 
-func (db *DBAPI) a(funcName string, params ...interface{}) (bool, JSONResult) {
+func (db *DBAPI) a(funcName string, params ...interface{}) (bool, []byte) {
 	qs := "("
 	for i := range params {
 		if i > 0 {
@@ -44,57 +44,108 @@ func (db *DBAPI) a(funcName string, params ...interface{}) (bool, JSONResult) {
 	sql := fmt.Sprintf("SELECT ok, js FROM %s.%s%s", db.schema, funcName, qs)
 
 	var ok bool
-	var js []byte
+	var js []byte // valid json from postgress
 
 	err := db.conn.QueryRow(context.Background(), sql, params...).Scan(&ok, &js)
 	if err != nil {
-		return false, JSONResult{}
+		return false, nil
 	}
 
-	var result interface{}
-	err = json.Unmarshal(js, &result)
+	return true, js
+}
+
+// generic function to decode json
+func decodeJSON[T any](data []byte) (T, error) {
+	var result T
+	var temp interface{}
+	if err := json.Unmarshal(data, &temp); err != nil {
+		return result, err
+	}
+
+	decoded, err := castToType[T](temp)
 	if err != nil {
-		fmt.Println(err)
+		return result, err
 	}
 
-	switch result.(type) {
-	case []interface{}:
-		// convert []interface{} to []map[string]interface{}
-		var array []map[string]interface{}
-		for _, item := range result.([]interface{}) {
-			array = append(array, item.(map[string]interface{}))
-		}
-		return true, JSONResult{A: array}
+	return decoded, nil
+}
+
+// cast the decoded json to the desired type
+func castToType[T any](data interface{}) (T, error) {
+	var result T
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		return result, err
+	}
+
+	err = json.Unmarshal(bytes, &result)
+	return result, err
+}
+
+func getNestedField[T any](data interface{}, keys ...string) (T, error) {
+	var result T
+
+	if len(keys) == 0 {
+		return castToType[T](data)
+	}
+
+	switch d := data.(type) {
 	case map[string]interface{}:
-		return true, JSONResult{O: result.(map[string]interface{})}
+		if val, found := d[keys[0]]; found {
+			return getNestedField[T](val, keys[1:]...)
+		} else {
+			return result, fmt.Errorf("key %s not found", keys[0])
+		}
+	case []interface{}:
+		var results []T
+		for _, item := range d {
+			result, err := getNestedField[T](item, keys...)
+			if err != nil {
+				return result, err
+			}
+			results = append(results, result)
+		}
+		return castToType[T](results)
+	default:
+		return result, fmt.Errorf("unexpected type %T", d)
 	}
-
-	return false, JSONResult{}
 }
 
 func main() {
 	API := NewDBAPI("zz")
 
-	fmt.Println("PERSON:")
-	_, person := API.a("person", 1)
-	fmt.Println(person.O["name"])
-	_, person = API.a("person", 2)
-	fmt.Println(person.O["name"])
-	_, person = API.a("person", 999)
-	fmt.Println(person.O["error"])
-
 	fmt.Println("PEOPLE:")
-	_, people := API.a("people")
-	fmt.Println(people.A[0]["name"])
+	ok, jsonData := API.a("people")
+	if ok {
+		var result []map[string]interface{}
+		result, err := decodeJSON[[]map[string]interface{}](jsonData)
+		if err != nil {
+			fmt.Println("Error decoding JSON:", err)
+		} else {
+			fmt.Println("Decoded JSON:", result[0]["name"])
+		}
+	}
 
-	fmt.Println("GIVE CHARLIE CHOCOLATE: (only once)")
-	_, thing := API.a("thing_add", 2, "chocolate", 3.75)
-	fmt.Println(thing)
+	fmt.Println("PERSON:")
+	ok, jsonData = API.a("person", 1)
+	if ok {
+		var result map[string]interface{}
+		result, err := decodeJSON[map[string]interface{}](jsonData)
+		if err != nil {
+			fmt.Println("Error decoding JSON:", err)
+		} else {
+			fmt.Println("Decoded JSON:", result)
 
-	_, thing = API.a("thing_add", 2, "chocolate", 3.75)
-	fmt.Println(thing)
-
-	fmt.Println("GIVE CHARLIE LOVE: (overloaded, priceless)")
-	_, thing = API.a("thing_add", 2, "love")
-	fmt.Println(thing)
+			// getting the things
+			var things []map[string]interface{}
+			things, err := getNestedField[[]map[string]interface{}](result, "things")
+			if err != nil {
+				fmt.Println("Error accessing nested field:", err)
+			} else {
+				for i, thing := range things {
+					fmt.Printf("%d -> %v\n", i, thing["name"])
+				}
+			}
+		}
+	}
 }
